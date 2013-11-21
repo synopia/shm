b1ad31ca3ff4f397563e42c7869db1c0ef48ea24
Paul Fritsche <paul.fritsche@gmail.com>
2013-11-22 00:13:36 +0100

    login & bewertungsauswahl

--- data.rb
+++ data.rb
@@ -1,31 +1,6 @@
 
+
 # Uni Mannschaft Name Leistungsklasse Riege Geschlecht
-add("TUD",1,"Seppl","KM2",1,'m')
-add("TUD",1,"Horst","KM3",2,'w')
+Turner.add("TUD",1,"Seppl","KM2",1,'m')
+Turner.add("TUD",1,"Horst","KM3",2,'w')
 
-def add( uni, mannschaft, name, km, riege, geschlecht )
-  turner = {
-      :uni => uni,
-      :mannschaft => mannschaft,
-      :name => name,
-      :km => km,
-      :riege => riege,
-      :geschlecht => geschlecht,
-      :wertungen => {
-          :boden => -1,
-          :pauschenpferd => -1,
-          :ringe => -1,
-          :sprung => -1,
-          :barren => -1,
-          :reck => -1,
-          :stufenbarren => -1,
-          :schwebebalken => -1
-      }
-  }
-  # turner ins array einfuegen
-end
-
-def competitors
-  [
-  ]
-end
--- shm.rb
+++ shm.rb
@@ -1,26 +1,80 @@
 require 'sinatra'
+
+class Turner
+  def self.add( uni, mannschaft, name, km, riege, geschlecht )
+    @all ||= []
+    @all << {
+        :uni => uni,
+        :mannschaft => mannschaft,
+        :name => name,
+        :km => km,
+        :riege => riege,
+        :geschlecht => geschlecht,
+        :wertungen => {
+            :boden => -1,
+            :pauschenpferd => -1,
+            :ringe => -1,
+            :sprung => -1,
+            :barren => -1,
+            :reck => -1,
+            :stufenbarren => -1,
+            :schwebebalken => -1
+        }
+    }
+  end
+
+  def self.all
+    @all
+  end
+end
+
 require_relative 'data'
 
-@all_turner = turner()
-@all_geraete = geraete()
+class Kari
+  def self.add name
+    @all ||= []
+    @all << { :name => name }
+  end
 
-def filter_turner( )
-  @all_turner
+  def self.all
+    @all
+  end
 end
 
-def filter_geraete()
-  @all_geraete
+Kari.add "lisa"
+Kari.add "maggy"
+Kari.add "bart"
+Kari.add "homer"
+
+class Geraet
+  def self.add name
+    @all ||= []
+    @all << { :name=>name }
+  end
+
+  def self.all
+    @all
+  end
 end
+
+Geraet.add :boden
+Geraet.add :pauschenpferd
+Geraet.add :ringe
+Geraet.add :sprung
+Geraet.add :barren
+Geraet.add :reck
+Geraet.add :stufenbarren
+Geraet.add :schwebebalken
 
 def login
-  karis()[@params[:kari].to_i]
+  Kari.all()[@params[:kari].to_i]
 end
 
 get '/' do
-  erb :login, :locals=>{:karis=>karis()}
+  erb :login, :locals=>{:karis=>Kari.all}
 end
 
 post '/login' do
 
-  erb :bewerten, :locals=>{:kari=>login() }
+  erb :bewerten, :locals=>{:kari=>login(), :geraete=>Geraet.all, :turners=>Turner.all }
 end
