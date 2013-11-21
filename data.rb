ad7b697806235bd08a3bce74b849c5ab6b5bff48
Paul Fritsche <paul.fritsche@gmail.com>
2013-11-21 23:05:44 +0100

    initial commit

---
+++ data.rb
@@ -0,0 +1,27 @@
+def competitors
+  [
+      {
+          :uni => "TUD",
+          :name => "Seppl",
+          :riege => 10,
+          :lkl => 10
+      },
+      {
+          :uni => "TUD",
+          :name => "Aprok",
+          :riege => 102,
+          :lkl => 120
+      }
+  ]
+end
+
+def tools
+  [
+      {
+          :name=>"Boden",
+      },
+      {
+          :name=>"Reck",
+      }
+  ]
+end
