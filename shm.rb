207d3ab965323b2a45abc2bcbd4d0c3604b1e6f5
Paul Fritsche <paul.fritsche@gmail.com>
2013-11-21 23:52:17 +0100

    bewertung

--- shm.rb
+++ shm.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
 require_relative 'data'
 
+@all_turner = turner()
+@all_geraete = geraete()
 
+def filter_turner( )
+  @all_turner
+end
+
+def filter_geraete()
+  @all_geraete
+end
+
+def login
+  karis()[@params[:kari].to_i]
+end
 
 get '/' do
   erb :login, :locals=>{:karis=>karis()}
+end
+
+post '/login' do
+
+  erb :bewerten, :locals=>{:kari=>login() }
 end
