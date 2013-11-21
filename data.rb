552a363f0f9f8aaa3a1b422bad8f9de6b0e3f432
albrecht <albrecht.uhlig@gmx.de>
2013-11-22 00:24:51 +0100

    turner in daten eingtragen

--- data.rb
+++ data.rb
@@ -1,31 +1,44 @@
-
 # Uni Mannschaft Name Leistungsklasse Riege Geschlecht
-add("TUD",1,"Seppl","KM2",1,'m')
-add("TUD",1,"Horst","KM3",2,'w')
 
-def add( uni, mannschaft, name, km, riege, geschlecht )
-  turner = {
-      :uni => uni,
-      :mannschaft => mannschaft,
-      :name => name,
-      :km => km,
-      :riege => riege,
-      :geschlecht => geschlecht,
-      :wertungen => {
-          :boden => -1,
-          :pauschenpferd => -1,
-          :ringe => -1,
-          :sprung => -1,
-          :barren => -1,
-          :reck => -1,
-          :stufenbarren => -1,
-          :schwebebalken => -1
-      }
-  }
-  # turner ins array einfuegen
-end
+# Tu Dresden
+Turner.add("TU-Dresden",1,"Bochmann, Jakob",     "KM2",1,'m')
+Turner.add("TU-Dresden",1,"Bölke, Nico",         "KM3",1,'m')
+Turner.add("TU-Dresden",1,"Hamann, Stefanie",    "KM2",1,'w')
+Turner.add("TU-Dresden",1,"Klause, Fabian",      "KM2",1,'m')
+Turner.add("TU-Dresden",2,"Bedrich, Annelie",    "KM3",1,'w')
+Turner.add("TU-Dresden",2,"Kalz, Julia",         "KM4",1,'w')
+Turner.add("TU-Dresden",2,"Nacke, Barbara",      "KM3",1,'w')
+Turner.add("TU-Dresden",2,"Schaunsland, Kelly",  "KM3",1,'w')
+Turner.add("TU-Dresden",3,"Kemnitz, Alexander",  "KM4",1,'m')
+Turner.add("TU-Dresden",3,"Kubicova, Marie",     "KM4",1,'w')
 
-def competitors
-  [
-  ]
-end
+# TU Freiberg
+Turner.add("TU-Freiberg",1,"Pollender, André",   "KM3",1,'m')
+Turner.add("TU-Freiberg",1,"Jäckel, Philipp",    "KM4",1,'m')
+Turner.add("TU-Freiberg",1,"Haupt, Amelie",      "KM4",1,'w')
+Turner.add("TU-Freiberg",1,"Schulze, Caroline",  "KM4",1,'w')
+# Ersatz Schumann, Julian
+
+# Uni Leipzig
+Turner.add("Uni-Leipzig",1,"Schmidt, Susi",      "KM3",1,'w')
+Turner.add("Uni-Leipzig",1,"Paschmann, Lisa",    "KM3",1,'w')
+Turner.add("Uni-Leipzig",1,"Holstein, Miriam",   "KM3",1,'w')
+Turner.add("Uni-Leipzig",1,"Haubner, Stephanie", "KM3",1,'w')
+
+# HTW Dresden
+Turner.add("HTW-Dresden",1,"Leucht, Vivien",     "KM4",1,'w')
+Turner.add("HTW-Dresden",1,"Scharf, Alexandra",  "KM4",1,'w')
+
+# TU Chemnitz
+Turner.add("TU-Chemnitz",1,"Uhle, Andre",        "KM3",1,'w')
+Turner.add("TU-Chemnitz",1,"Bauer, David",       "KM3",1,'w')
+Turner.add("TU-Chemnitz",1,"Treffkorn, Nico",    "KM3",1,'w')
+Turner.add("TU-Chemnitz",1,"Thiele, Jan",        "KM3",1,'w')
+Turner.add("TU-Chemnitz",2,"Henning, Sophie",    "KM3",1,'w')
+Turner.add("TU-Chemnitz",2,"Engel-Kreher, Anna", "KM3",1,'w')
+Turner.add("TU-Chemnitz",2,"Rubi, Regina",       "KM3",1,'w')
+Turner.add("TU-Chemnitz",2,"Müller, Luise",      "KM3",1,'w')
+
+# BA Glauchau
+Turner.add("BA-Glauchau",1,"Kirchberger, Klaus", "KM2",1,'m')
+
