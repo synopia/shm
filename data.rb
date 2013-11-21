3455a6d6c04bc27a1e9142ad85fd54f1783a9beb
albrecht <albrecht.uhlig@gmx.de>
2013-11-21 23:33:58 +0100

    datenstruktur

--- data.rb
+++ data.rb
@@ -1,26 +1,17 @@
-def competitors
-  [
-      {
-          :uni => "TUD",
-          :name => "Seppl",
-          :riege => 1,
-          :km => 2,
-          :geschlecht => 'w',
-          :boden => -1,
-          :pauschenpferd => -1,
-          :ringe => -1,
-          :sprung => -1,
-          :barren => -1,
-          :reck => -1,
-          :stufenbarren => -1,
-          :schwebebalken => -1
-      },
-      {
-          :uni => "TUD",
-          :name => "Aprok",
-          :riege => 2,
-          :km => 3,
-          :geschlecht => 'm',
+
+# Uni Mannschaft Name Leistungsklasse Riege Geschlecht
+add("TUD",1,"Seppl","KM2",1,'m')
+add("TUD",1,"Horst","KM3",2,'w')
+
+def add( uni, mannschaft, name, km, riege, geschlecht )
+  turner = {
+      :uni => uni,
+      :mannschaft => mannschaft,
+      :name => name,
+      :km => km,
+      :riege => riege,
+      :geschlecht => geschlecht,
+      :wertungen => {
           :boden => -1,
           :pauschenpferd => -1,
           :ringe => -1,
@@ -30,5 +21,11 @@ def competitors
           :stufenbarren => -1,
           :schwebebalken => -1
       }
+  }
+  # turner ins array einfuegen
+end
+
+def competitors
+  [
   ]
 end
