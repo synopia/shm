a32626f015a03ed21954c91b5cbce37ce3e530f8
Paul Fritsche <paul.fritsche@gmail.com>
2013-11-22 15:05:23 +0100

    wertung speichern

--- shm.rb
+++ shm.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
+require 'json'
 
 set :bind, '0.0.0.0'
 
 $id = 1
 
 class Turner
-  def self.add( uni, mannschaft, name, km, riege, geschlecht, boden=-1, pauschenpferd=-1, ringe=-1, sprung=-1, barren=-1, reck=-1, stufenbarren=-1, schwebebalken=-1 )
+  def self.add( uni, mannschaft, name, km, riege, geschlecht )
     @all ||= []
     @all << {
         :id => $id,
@@ -15,18 +16,26 @@ class Turner
         :km => km,
         :riege => riege,
         :geschlecht => geschlecht,
-        #:wertungen => {
-            :boden => boden,
-            :pauschenpferd => pauschenpferd,
-            :ringe => ringe,
-            :sprung => sprung,
-            :barren => barren,
-            :reck => reck,
-            :stufenbarren => -stufenbarren,
-            :schwebebalken => schwebebalken
-        #}
+        :wertungen => {}
     }
     $id = $id + 1
+  end
+  def self.load_wertungen
+    begin
+      data = JSON.parse File.read('wertungen.json')
+      @all.each do |tu|
+        wertung = data.select{ |d| d[:id]==tu[:id]}.first
+        tu[:wertungen] = wertung unless wertung.nil?
+      end
+    rescue
+    end
+
+  end
+  def self.save_wertungen
+    wertungen = @all.collect {|tu| tu[:wertungen].merge({:id=>tu[:id]})}
+    File.open('wertungen.json', 'w') do |file|
+      file.write wertungen.to_json
+    end
   end
   def self.find_by_geschlecht geschlecht
     @all.select do |t|
@@ -39,11 +48,7 @@ class Turner
     end
   end
   def self.find_by_id id
-    @all.each do |t|
-      if t[:id] == id
-        return t
-      end
-    end
+    @all.select { |t| t[:id]==id }.first
   end
 
 
@@ -55,45 +60,41 @@ end
 
 require_relative 'data'
 
+Turner.load_wertungen
+
 enable :sessions
-def login
-  session[:user_id] = @params[:kari].to_i unless @params[:kari].nil?
-  Kari.all()[session[:user_id]]
-end
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
-get '/' do
-  erb :login, :layout => :layout, :locals=>{:karis=>Kari.all}
+helpers do
+  def update_turner
+    session[:geraet] = @params[:geraet]
+    tu = Turner.find_by_id(@params[:turner].to_i)
+    unless tu.nil?
+      tu[:wertungen][@params[:geraet]] = @params[:wertung].to_f
+      Turner.save_wertungen
+    end
+  end
 end
 
-post '/login' do
-  kari = login()
-  if kari.nil?
-    redirect '/'
-  else
-    redirect '/wertung'
-  end
+get '/' do
+  erb :login, :layout => :layout
 end
 
 get '/frauen' do
   riege3 = Turner.find_by_riege(3)
   riege4 = Turner.find_by_riege(4)
-
-  erb :frauen, :layout => :layout, :locals=>{:riege3=>riege3, :riege4=>riege4  }
+  puts session[:geraet]
+  erb :frauen, :layout => :layout, :locals=>{:riege3=>riege3, :riege4=>riege4, :geraet=>session[:geraet]  }
 end
 
 post '/frauen' do
-  puts @params.to_s
-  tu = Turner.find_by_id(@params[:turner].to_i)
-  tu[@params[:geraet].to_sym] = @params[:wertung].to_f
+  update_turner
   redirect '/frauen'
 end
 
 post '/maenner' do
-  puts @params.to_s
-  tu = Turner.find_by_id(@params[:turner].to_i)
-  tu[@params[:geraet].to_sym] = @params[:wertung].to_f
+  update_turner
   redirect '/maenner'
 end
 
@@ -101,7 +102,7 @@ get '/maenner' do
   riege1 = Turner.find_by_riege(1)
   riege2 = Turner.find_by_riege(2)
 
-  erb :maenner, :layout => :layout, :locals=>{:riege1=>riege1, :riege2=>riege2  }
+  erb :maenner, :layout => :layout, :locals=>{:riege1=>riege1, :riege2=>riege2, :geraet=>session[:geraet]  }
 end
 
 get '/einzelwertungen' do
