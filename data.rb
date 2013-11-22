57d004294a8bd0532a721fa800c368a31e672a4f
Paul Fritsche <paul.fritsche@gmail.com>
2013-11-22 15:37:50 +0100

    wertung speichern

--- data.rb
+++ data.rb
@@ -3,47 +3,47 @@
 # Uni Mannschaft Name Leistungsklasse Riege Geschlecht
 
 # Tu Dresden
-Turner.add("TU-Dresden",1,"Jakob Bochmann",     "KM2",1,'m')
-Turner.add("TU-Dresden",1,"Nico Bölke",         "KM3",1,'m')
-Turner.add("TU-Dresden",1,"Stefanie Hamann",    "KM2",3,'w')
-Turner.add("TU-Dresden",1,"Fabian Klause",      "KM2",1,'m')
-Turner.add("TU-Dresden",2,"Annelie Bedrich",    "KM3",3,'w')
-Turner.add("TU-Dresden",2,"Julia Kalz",         "KM4",3,'w')
-Turner.add("TU-Dresden",2,"Barbara Nacke",      "KM3",3,'w')
-Turner.add("TU-Dresden",2,"Kelly Schaunsland",  "KM3",3,'w')
-Turner.add("TU-Dresden",3,"Alexander Kemnitz",  "KM4",2,'m')
-Turner.add("TU-Dresden",3,"Marie Kubicova",     "KM4",4,'w')
+Turner.add( 1, "TU-Dresden",1,"Jakob Bochmann",     "KM2",1,'m')
+Turner.add( 2, "TU-Dresden",1,"Nico Bölke",         "KM3",1,'m')
+Turner.add( 3, "TU-Dresden",1,"Stefanie Hamann",    "KM2",3,'w')
+Turner.add( 4, "TU-Dresden",1,"Fabian Klause",      "KM2",1,'m')
+Turner.add( 5, "TU-Dresden",2,"Annelie Bedrich",    "KM3",3,'w')
+Turner.add( 6, "TU-Dresden",2,"Julia Kalz",         "KM4",3,'w')
+Turner.add( 7, "TU-Dresden",2,"Barbara Nacke",      "KM3",3,'w')
+Turner.add( 8, "TU-Dresden",2,"Kelly Schaunsland",  "KM3",3,'w')
+Turner.add( 9, "TU-Dresden",3,"Alexander Kemnitz",  "KM4",2,'m')
+Turner.add(10, "TU-Dresden",3,"Marie Kubicova",     "KM4",4,'w')
 
 # TU Freiberg
-Turner.add("TU-Freiberg",1,"André Pollender",   "KM3",1,'m')
-Turner.add("TU-Freiberg",1,"Philipp Jäckel",    "KM4",1,'m')
-Turner.add("TU-Freiberg",1,"Amelie Haupt",      "KM4",4,'w')
-Turner.add("TU-Freiberg",1,"Caroline Schulze",  "KM4",4,'w')
+Turner.add(11, "TU-Freiberg",1,"André Pollender",   "KM3",1,'m')
+Turner.add(12, "TU-Freiberg",1,"Philipp Jäckel",    "KM4",1,'m')
+Turner.add(13, "TU-Freiberg",1,"Amelie Haupt",      "KM4",4,'w')
+Turner.add(14, "TU-Freiberg",1,"Caroline Schulze",  "KM4",4,'w')
 # Ersatz Schumann, Julian
 
 # Uni Leipzig
-Turner.add("Uni-Leipzig",1,"Susi Schmidt",      "KM3",3,'w')
-Turner.add("Uni-Leipzig",1,"Lisa Paschmann",    "KM3",3,'w')
-Turner.add("Uni-Leipzig",1,"Miriam Holstein",   "KM3",3,'w')
-Turner.add("Uni-Leipzig",1,"Stephanie Haubner", "KM3",3,'w')
+Turner.add(15, "Uni-Leipzig",1,"Susi Schmidt",      "KM3",3,'w')
+Turner.add(16, "Uni-Leipzig",1,"Lisa Paschmann",    "KM3",3,'w')
+Turner.add(17, "Uni-Leipzig",1,"Miriam Holstein",   "KM3",3,'w')
+Turner.add(18, "Uni-Leipzig",1,"Stephanie Haubner", "KM3",3,'w')
 
 # HTW Dresden
-Turner.add("HTW-Dresden",1,"Vivien Leucht",     "KM4",4,'w')
-Turner.add("HTW-Dresden",1,"Alexandra Scharf",  "KM4",4,'w')
+Turner.add(19, "HTW-Dresden",1,"Vivien Leucht",     "KM4",4,'w')
+Turner.add(20, "HTW-Dresden",1,"Alexandra Scharf",  "KM4",4,'w')
 
 # TU Chemnitz
-Turner.add("TU-Chemnitz",1,"Andre Uhle",        "KM3",2,'m')
-Turner.add("TU-Chemnitz",1,"David Bauer",       "KM4",2,'m')
-Turner.add("TU-Chemnitz",1,"Nico Treffkorn",    "KM4",2,'m')
-Turner.add("TU-Chemnitz",1,"Jan Thiele",        "KM4",2,'m')
-Turner.add("TU-Chemnitz",2,"Sophie Henning",    "KM3",4,'w')
-Turner.add("TU-Chemnitz",2,"Anna Engel-Kreher", "KM3",4,'w')
-Turner.add("TU-Chemnitz",2,"Regina Rubi",       "KM3",4,'w')
-Turner.add("TU-Chemnitz",2,"Luise Müller",      "KM3",4,'w')
+Turner.add(21, "TU-Chemnitz",1,"Andre Uhle",        "KM3",2,'m')
+Turner.add(22, "TU-Chemnitz",1,"David Bauer",       "KM4",2,'m')
+Turner.add(23, "TU-Chemnitz",1,"Nico Treffkorn",    "KM4",2,'m')
+Turner.add(24, "TU-Chemnitz",1,"Jan Thiele",        "KM4",2,'m')
+Turner.add(25, "TU-Chemnitz",2,"Sophie Henning",    "KM3",4,'w')
+Turner.add(26, "TU-Chemnitz",2,"Anna Engel-Kreher", "KM3",4,'w')
+Turner.add(27, "TU-Chemnitz",2,"Regina Rubi",       "KM3",4,'w')
+Turner.add(28, "TU-Chemnitz",2,"Luise Müller",      "KM3",4,'w')
 
 # BA Glauchau
-Turner.add("BA-Glauchau",1,"Klaus Kirchberger", "KM2",1,'m')
+Turner.add(29, "BA-Glauchau",1,"Klaus Kirchberger", "KM2",1,'m')
 
 # Hochschule Mittweida
-Turner.add("HS-Mittweida",1,"Martin Sonntag",   "KM4",2,'m')
+Turner.add(30, "HS-Mittweida",1,"Martin Sonntag",   "KM4",2,'m')
 
--- shm.rb
+++ shm.rb
@@ -3,23 +3,21 @@ require 'json'
 
 set :bind, '0.0.0.0'
 
-$id = 1
-
 class Turner
-  def self.add( uni, mannschaft, name, km, riege, geschlecht )
+  def self.add( id, uni, mannschaft, name, km, riege, geschlecht )
     @all ||= []
     @all << {
-        :id => $id,
+        :id => id,
         :uni => uni,
         :mannschaft => mannschaft,
         :name => name,
         :km => km,
         :riege => riege,
         :geschlecht => geschlecht,
-        :wertungen => {}
+        :wertungen => { :id=>id }
     }
-    $id = $id + 1
   end
+
   def self.load_wertungen
     begin
       data = JSON.parse File.read('wertungen.json')
@@ -29,34 +27,31 @@ class Turner
       end
     rescue
     end
-
   end
+
   def self.save_wertungen
-    wertungen = @all.collect {|tu| tu[:wertungen].merge({:id=>tu[:id]})}
+    wertungen = @all.collect {|tu| tu[:wertungen] }
     File.open('wertungen.json', 'w') do |file|
       file.write wertungen.to_json
     end
   end
+
   def self.find_by_geschlecht geschlecht
-    @all.select do |t|
-      t[:geschlecht].include? geschlecht
-    end
+    @all.select { |t| t[:geschlecht].include? geschlecht }
   end
+
   def self.find_by_riege riege
-    @all.select do |t|
-      t[:riege] == riege
-    end
+    @all.select { |t| t[:riege] == riege }
   end
+
   def self.find_by_id id
     @all.select { |t| t[:id]==id }.first
   end
-
 
   def self.all
     @all
   end
 end
-
 
 require_relative 'data'
 
@@ -71,9 +66,13 @@ helpers do
     session[:geraet] = @params[:geraet]
     tu = Turner.find_by_id(@params[:turner].to_i)
     unless tu.nil?
-      tu[:wertungen][@params[:geraet]] = @params[:wertung].to_f
+      tu[:wertungen][@params[:geraet]] = @params[:wertung].gsub(',','.').to_f
       Turner.save_wertungen
     end
+  end
+
+  def bewertung( geschlecht, riege_a, riege_b, geraete )
+    erb :bewertung, :layout => :layout, :locals=>{:riege_a=>riege_a, :riege_b=>riege_b, :geraet=>session[:geraet], :geraete=>geraete, :geschlecht=>geschlecht  }
   end
 end
 
@@ -81,11 +80,12 @@ get '/' do
   erb :login, :layout => :layout
 end
 
+get '/maenner' do
+  bewertung( :maenner, 1, 2, %w(Boden Pauschenpferd Ringe Sprung Barren Reck))
+end
+
 get '/frauen' do
-  riege3 = Turner.find_by_riege(3)
-  riege4 = Turner.find_by_riege(4)
-  puts session[:geraet]
-  erb :frauen, :layout => :layout, :locals=>{:riege3=>riege3, :riege4=>riege4, :geraet=>session[:geraet]  }
+  bewertung( :frauen, 3, 4, %w(Sprung Stufenbarren Schwebebalken Boden))
 end
 
 post '/frauen' do
@@ -96,13 +96,6 @@ end
 post '/maenner' do
   update_turner
   redirect '/maenner'
-end
-
-get '/maenner' do
-  riege1 = Turner.find_by_riege(1)
-  riege2 = Turner.find_by_riege(2)
-
-  erb :maenner, :layout => :layout, :locals=>{:riege1=>riege1, :riege2=>riege2, :geraet=>session[:geraet]  }
 end
 
 get '/einzelwertungen' do
